062b4060485b8e9abd411a7fe9de8385a907ae19
Kuldeep Singh <in.singhkuldeep@gmail.com>
2021-06-06 17:19:18 +0100

    New project setup

---
+++ IdentityAuthentication/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdentityAuthentication.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityAuthUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //builder.Entity<WorkItem>().HasKey(a => a.WorkItemId);
+            //builder.Entity<Team>().HasKey(a => a.TeamId);
+
+            base.OnModelCreating(builder);
+
+        }
+    }
+}
---
+++ IdentityAuthentication/Data/IdentityAuthUser.cs
@@ -0,0 +1,10 @@
+﻿
+namespace IdentityAuthentication.Data
+{
+    using Microsoft.AspNetCore.Identity;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class IdentityAuthUser : IdentityUser
+    {
+    }
+}
